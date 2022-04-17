763004b5874bf1cc900b5a51dd9938f8d868e788
Artem Kyznecov <soldatenco96@mail.ru>
2022-04-17 16:04:42 +0300

    Dice page added

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
 from random import randint
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 # route - путь
 @app.route("/")
 def index():
+    return render_template("index.html")
+
+
+@app.route("/dice")
+def dice():
     number = randint(1, 6)
-    return f"<h1>Вы бросили кубик, выпало {number}</h1>"
+    return render_template("dice.html", result=number)
+
 
 app.run()
